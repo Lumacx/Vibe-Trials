664caeeed8e9cde28db5eb827df1d3ab914e7b53
Lumacx <lumacx@gmail.com>
2025-08-01 00:36:20 +0000

    import next in functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,13 @@
 // functions/src/index.ts
-
 import * as functions from 'firebase-functions';
-import express, { Request, Response } from 'express';
+import next from 'next';
+import { Request, Response } from 'express';
 
-const app = express();
+const dev = process.env.NODE_ENV !== 'production';
+const nextApp = next({ dev, conf: { distDir: '.next' } }); // ✅ importante para App Hosting
+const handle = nextApp.getRequestHandler();
 
-app.get('/hello', (req: Request, res: Response) => {
-  res.send('Hello from Firebase Functions + Express!');
+export const nextServer = functions.https.onRequest(async (req: Request, res: Response) => {
+  await nextApp.prepare();
+  handle(req, res);
 });
-
-export const api = functions.https.onRequest(app);
